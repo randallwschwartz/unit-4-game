00e34e50f9680f87175139bbd2737128f82a9d55
rschwartz <37844284+randallwschwartz@users.noreply.github.com>
2018-06-03 21:35:23 -0500

    6/3 21.35 rws

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// Javascript for Collect Crystals
+
+var RandomNumber = document.getElementById("RandomNumber");
+var wins = document.getElementById("numWins");
+var losses = document.getElementById("numLosses");
+var totalScore = document.getElementById("TotalScore");
+
+var wordArray = [
+"krieger"
+];
+
+
+losses = 0;
+
